ac8189f9923a92ad976ff0137bad924e5b343881
Viv-ek-Thak-ur <vivekchitti123@gmail.com>
2026-07-19 11:54:27 +0530

    Navbar Responsive Design and Topbar Animation

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,14 @@
 import { useNavigate, Link } from "react-router-dom";
 import logo from "../../assets/Group 8.svg";
 import styles from "./Navbar.module.css";
-
+import { useState } from "react";
+import MenuIcon from "../../assets/burger-menu-right-svgrepo-com.svg";
 
 
 export default function Navbar(){
+
+  const [menuOpen,setMenuOpen] =useState(false)
+
    const navs = [
   { label: "Find Doctors", path: "/" },
   { label: "Hospitals", path: "/search-results" },
@@ -17,14 +21,19 @@ const navigate = useNavigate();
 
 const handleMyBookings = () => {
   navigate(`/my-bookings`);
+
 }
+
+
 
    return (
   <>
     <div className={styles.topBar}>
-      The health and well-being of our patients and their health care team
-      will always be our priority, so we follow the best practices for
-      cleanliness.
+       <span className={styles.topBarText}>
+          The health and well-being of our patients and their health care team
+          will always be our priority, so we follow the best practices for
+          cleanliness.
+        </span>
     </div>
 
     <nav className={styles.navbar}>
@@ -32,8 +41,15 @@ const handleMyBookings = () => {
         <Link to="/" className={styles.logo}>
           <img src={logo} alt="Medify Logo" />
         </Link>
+        <div className={menuOpen?styles.hamExpand:""}>
+        <button
+          className={styles.hamburger}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {menuOpen ? "✕": <img src = {MenuIcon} alt = "Menu" className={styles.menuIcon}/>}
+        </button>
 
-        <div className={styles.navLinks}>
+        <div className={`${styles.navLinks} ${menuOpen?styles.showMenu:""}`}>
           {navs.map((nav) => (
             <Link
               key={nav.label}
@@ -43,15 +59,18 @@ const handleMyBookings = () => {
               {nav.label}
             </Link>
           ))}
-        </div>
-
-        <button
+          <button
           type="button"
           onClick={handleMyBookings}
           className={styles.bookingBtn}
         >
           My Bookings
         </button>
+        </div>
+     
+        </div>
+
+        
       </div>
     </nav>
   </>
